28b05655a9176fb414c54e42b1747a81e82811e6
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-05-02 00:19:51 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+
+function findMatching(drivers,driverName){
+    return drivers.filter(function (elementName){return elementName === driverName || elementName === driverName.toLowerCase()})
+}
+
+function fuzzyMatch(drivers, nameLetters){
+    return drivers.filter(function (elementName){return elementName.startsWith(nameLetters) || (elementName.toLowerCase()).startsWith(nameLetters)})
+}
+
+function matchName(drivers, driverName){
+    return drivers.filter(function (driverObject){return driverObject.name === driverName})
+}
